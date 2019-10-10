1994f87ba7776cf9594c6f8beb0b4062cd25df7b
zolga <olga.kolopanov@gmail.com>
2019-10-10 12:42:05 -0700

    Add TODO

--- backend/constants.js
+++ backend/constants.js
@@ -1,5 +1,5 @@
 /**
- * This file will be remove when import from src folder will be supported
+ * TODO: This file will be remove when import from src folder will be supported
  */
 export const errorType = {
   FETCH: 'error fetch data',
